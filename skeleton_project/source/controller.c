8637af6b1c37bbbe0faeaf61030d82d8fcd3ec23
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-20 20:23:59 +0100

    Tanker om kontrolleren

--- skeleton_project/source/controller.c
+++ skeleton_project/source/controller.c
@@ -7,6 +7,16 @@ typedef struct state{
     IDLE
     GOING_UP
     GOING_DOWN
-    AT_TOP
-    AT_BUTTON
+    AT_TOP          //Unused
+    AT_BUTTON       //Unused
 }state;
+
+
+
+//Controlleren må være "føleren" som skal kjenne på sensorene til en hver tid.
+//Må ta i bruk ordre for å heise heisen i den retningen som er ønskelig.
+
+//Trenger å initialize
+
+
+//Polle etter 
