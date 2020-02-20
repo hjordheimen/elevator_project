eb210d90118da42b10d335e1dc083dcd535e29cc
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-20 19:47:36 +0100

    Laget alle filer, gjort klar dependencies

---
+++ skeleton_project/source/controller.c
@@ -0,0 +1,9 @@
+
+
+typedef struct state{
+    IDLE
+    GOING_UP
+    GOING_DOWN
+    AT_TOP
+    AT_BUTTON
+}state;
--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -19,73 +19,7 @@ static void clear_all_order_lights(){
     }
 }
 
-// A structure to represent a queue
-struct Queue
-{
-    int front, rear, size;
-    unsigned capacity;
-    int* array;
-};
 
-// function to create a queue of given capacity.
-// It initializes size of queue as 0
-struct Queue* createQueue(unsigned capacity)
-{
-    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
-    queue->capacity = capacity;
-    queue->front = queue->size = 0;
-    queue->rear = capacity - 1;  // This is important, see the enqueue
-    queue->array = (int*) malloc(queue->capacity * sizeof(int));
-    return queue;
-}
-
-// Queue is full when size becomes equal to the capacity
-int isFull(struct Queue* queue)
-{  return (queue->size == queue->capacity);  }
-
-// Queue is empty when size is 0
-int isEmpty(struct Queue* queue)
-{  return (queue->size == 0); }
-
-// Function to add an item to the queue.
-// It changes rear and size
-void enqueue(struct Queue* queue, int item)
-{
-    if (isFull(queue))
-        return;
-    queue->rear = (queue->rear + 1)%queue->capacity;
-    queue->array[queue->rear] = item;
-    queue->size = queue->size + 1;
-    printf("%d enqueued to queue\n", item);
-}
-
-// Function to remove an item from queue.
-// It changes front and size
-int dequeue(struct Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    int item = queue->array[queue->front];
-    queue->front = (queue->front + 1)%queue->capacity;
-    queue->size = queue->size - 1;
-    return item;
-}
-
-// Function to get front of queue
-int front(struct Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->front];
-}
-
-// Function to get rear of queue
-int rear(struct Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->rear];
-}
 
 
 
@@ -128,7 +62,6 @@ int main(){
         for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
             if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
                 hardware_command_floor_indicator_on(f);
-                enqueue(queue, f);
 
             }
         }
@@ -136,7 +69,6 @@ int main(){
         for (int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
             if(hardware_read_floor_sensor(f) == 1 && f == front(queue)){
                 hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-                dequeue(queue);
 
             }
         }
