e0db07d4966437f7d1c12f431bdf1b36a0b3aa6e
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-20 20:16:58 +0100

    Ideer til Orders

--- skeleton_project/source/order.c
+++ skeleton_project/source/order.c
@@ -1,1 +1,19 @@
 #include "queue.h"
+
+
+//Her må vi finne ut hvilke knapper som har blitt trykket, for så å sette den i
+// en kø,
+
+
+//Legg til i kø oppover
+
+
+
+//Legg til i kø nedover
+
+
+//Legg til i intern kø
+
+
+
+//Slett alle køer
