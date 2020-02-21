b069d790cbe29c41e7fbcf95fc08d55db8ece207
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 00:29:49 +0100

    Skisse for Orders

---
+++ skeleton_project/source/order.h
@@ -0,0 +1,24 @@
+
+//Bestill ordre oppover
+
+void add_order_up(int floor);
+
+//Bestill ordre nedover
+
+void add_order_down(int floor);
+
+//Hvor skal bestilingen
+
+void set_order(int floor);
+
+//Slett alle ordre
+
+void clear_all_orders();
+
+//Slett ordre
+
+void remove_order();
+
+//Ser om det er noen inkommende ordre.
+
+void look_for_orders();
