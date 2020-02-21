281ee7606aed7a006dcf15e9e294ed1f17e89306
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 00:47:54 +0100

    Skisse for queue satt

--- skeleton_project/source/queue.c
+++ skeleton_project/source/queue.c
@@ -5,16 +5,3 @@
 int volatile queue_up[3] = {0,0,0}; //Sorter liste for hver gang det kommer en ny bestilling.
 int volatile queue_inside[4] = {0,0,0,0};
 int volatile queue_down[3] = {0,0,0};//Sorter hver gang det kommer en ny bestilling.
-
-//Om den nye bestillingen er lavere enn dit du skal, stop og ta med,
-//Samme motstatt for motsatt vei
-
-
-//Sorter fra minst til størst, unntak er nullerne.
-
-
-
-//Sorter fra størst til minst
-
-
-//Ferdig med første element, Sett lik 0, for så å sortere. 
