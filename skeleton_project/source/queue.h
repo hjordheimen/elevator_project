8a65b773ca16033cc0dafbbc607dcdbb6c0564fc
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 00:44:17 +0100

    Skisse for queue satt

---
+++ skeleton_project/source/queue.h
@@ -0,0 +1,13 @@
+//Om den nye bestillingen er lavere enn dit du skal, stop og ta med,
+//Samme motstatt for motsatt vei
+
+
+//Sorter fra minst til størst, unntak er nullerne.
+void sort_queue(int up_down);
+
+
+//Sorter fra størst til minst
+
+
+//Ferdig med første element, Sett lik 0, for så å sortere. 
+void remove_element();
