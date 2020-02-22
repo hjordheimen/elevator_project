ad3c677d1ef6622bdfd4d220371c14cd7565b89e
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-23 00:51:41 +0100

    Lagt til tid, og hvordan døra skal åpnes og lukkes.

--- skeleton_project/source/controller.c
+++ skeleton_project/source/controller.c
@@ -1,8 +1,9 @@
 #include "hardware.h"
 #include "ordre.h"
 #include <stdio.h>
+#include <time.h>
 
-
+#define CLOSING_TIME 3
 #define BETWEEN_FLOORS -1
 
 static int current_floor = BETWEEN_FLOORS;
@@ -75,18 +76,20 @@ void halt(){
     switch (action) {
         case ENTER:
         hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-        //OPEN DOOR for 3 seconds, Sette på en Timer.
         hardware_command_floor_indicator_on(current_floor);
         clear_floor_orders(current_floor);
+        //OPEN DOOR for 3 seconds, Sette på en Timer.
+        hardware_command_door_open(1);
+        int door_time = time(NULL);
         next_state(HALT, INSIDE);
             break;
         case INSIDE:
         //Hvis timeren er ferdig, hopp til exit, der vi lukker døren, og går videre.
-
-
+            if(time(NULL) - door_time > CLOSING_TIME) next_state(HALT, EXIT);
             break;
         case EXIT:
         //Dørene lukkes.
+        hardware_command_door_open(0);
         update_next_order();
         if (get_next_order == -1)) {
             next_state(IDLE, ENTER);
