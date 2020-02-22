47ad8bcacd4eb23c9aa245aea9f38b66edf5efb0
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 23:05:42 +0100

    Controller logikk satt på

--- skeleton_project/source/controller.c
+++ skeleton_project/source/controller.c
@@ -6,34 +6,43 @@
 #define BETWEEN_FLOORS -1
 
 static int current_floor = BETWEEN_FLOORS;
-static HardwareMovement current_movement = HARDWARE_MOVEMENT_STOP;
+static HardwareMovement current_movement = HARDWARE_MOVEMENT_STOP; //Kanskje ha i orders
 static action action = ENTER;
 static state state = IDLE;
 
 
 void initialize(){
 
+
 }
 
 
 
 void idle(){
-    switch (action) {
-        case ENTER:
-            break;
-        case INSIDE:
-            break;
-        case EXIT:
-            break;
-        default:
-        break;
+    if (any_requests()) {
+        if (get_next_order < current_floor) {
+            next_state(GOING_DOWN, ENTER)
+        }
+        else next_state(GOING_UP, ENTER)
+    }
+
 }
 
 void go_up(){
     switch (action) {
         case ENTER:
+        hardware_command_movement(HARDWARE_MOVEMENT_UP);
+        next_state(GOING_UP, INSIDE);
             break;
         case INSIDE:
+            for (int floor = current_floor; floor < get_next_order; floor++){
+                if (check_floor_dir_value(floor, 0)){
+                    put_order_on_hold(floor);
+                }
+            }
+            if (current_floor == next_order) {
+                next_state(HALT, ENTER);
+            }
             break;
         case EXIT:
             break;
@@ -44,6 +53,8 @@ void go_up(){
 void go_down(){
     switch (action) {
         case ENTER:
+        hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+        next_state(GOING_DOWN, INSIDE);
             break;
         case INSIDE:
             break;
@@ -52,14 +63,33 @@ void go_down(){
         default:
         break;
 }
+}
 
 void halt(){
     switch (action) {
         case ENTER:
+        hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+        //OPEN DOOR for 3 seconds, Sette på en Timer.
+        clear_floor_orders(current_floor);
+        next_state(HALT, INSIDE);
             break;
         case INSIDE:
+        //Hvis timeren er ferdig, hopp til exit, der vi lukker døren, og går videre.
+
+
             break;
         case EXIT:
+        //Dørene lukkes.
+        update_next_order();
+        if (get_next_order == -1)) {
+            next_state(IDLE, ENTER);
+        }
+        else if(get_next_order < current_floor){
+            next_state(GOING_DOWN, ENTER);
+        }
+        else{
+            next_state(GOING_UP, ENTER);
+        }
             break;
         default:
         break;
