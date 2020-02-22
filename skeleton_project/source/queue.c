7097f8648024a2673b05332cdf62377bb1c4449e
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 14:06:40 +0100

    Gjør det mer brukenes til endringer

--- skeleton_project/source/queue.c
+++ skeleton_project/source/queue.c
@@ -1,7 +1,8 @@
 #include "controller.h"
 #include "ordre.h"
+#include "hardware.h"
 
 
-int volatile queue_up[3] = {0,0,0}; //Sorter liste for hver gang det kommer en ny bestilling.
-int volatile queue_inside[4] = {0,0,0,0};
-int volatile queue_down[3] = {0,0,0};//Sorter hver gang det kommer en ny bestilling.
+int volatile queue_up[HARDWARE_NUMBER_OF_FLOORS - 1] = {0,0,0}; //Sorter liste for hver gang det kommer en ny bestilling.
+int volatile queue_inside[HARDWARE_NUMBER_OF_FLOORS] = {0,0,0,0};
+int volatile queue_down[HARDWARE_NUMBER_OF_FLOORS - 1] = {0,0,0};//Sorter hver gang det kommer en ny bestilling.
