18955eb1979a820d2ede180515fb4b293c2ad635
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 20:25:26 +0100

    Think this is what I need for orders

--- skeleton_project/source/order.h
+++ skeleton_project/source/order.h
@@ -11,10 +11,6 @@ void next_order();
 
 void add_order_request(int floor, HardwareOrder order);
 
-//Hvor skal bestilingen
-
-void set_order(int floor);
-
 //Slett alle ordre på en gitt etasje
 
 void clear_floor_orders(int floor);
@@ -25,9 +21,6 @@ void clear_all_orders();
 
 //Ser om det er noen inkommende ordre.
 
-void look_for_requests();
-
-//Poller om det er noe signal å hente, både requests opp, ned, eller inni.
 
 void get_button_signal();
 
