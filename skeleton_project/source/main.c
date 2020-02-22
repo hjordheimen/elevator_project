b4cf4dd7ce1c8c60da9cc97e76c85e89a10bcec0
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-22 23:27:03 +0100

    Ryddet litt, må kunne lese knappene til enhver tid

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -41,44 +41,8 @@ int main(){
 
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
-    struct Queue* queue = createQueue(10);
-
-    hardware_command_movement(HARDWARE_MOVEMENT_UP);
 
     while(1){
-        if(hardware_read_stop_signal()){
-            hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-            break;
-        }
-
-        if(hardware_read_floor_sensor(0)){
-            hardware_command_movement(HARDWARE_MOVEMENT_UP);
-        }
-        if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
-            hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-        }
-
-        /* All buttons must be polled, like this: */
-        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
-                hardware_command_floor_indicator_on(f);
-
-            }
-        }
-
-        /*Changes the motordirection when the order is set up*/
-        for (int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
-                hardware_command_movement(HARDWARE_MOVEMENT_UP);
-            }
-        }
-        /*Changes the motordirection when the order is set down*/
-        for (int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
-                hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-            }
-        }
-
         /* Lights are set and cleared like this: */
         for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
             /* Internal orders */
@@ -121,6 +85,7 @@ int main(){
                 halt();
                 break;
         }
+        read_buttons();
     }
 
     return 0;
