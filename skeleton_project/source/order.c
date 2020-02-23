ff580410a68558df07afbe45cd8f210336e02b56
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-23 21:31:04 +0100

    Nå må vel alle lys bli slått av

--- skeleton_project/source/order.c
+++ skeleton_project/source/order.c
@@ -10,6 +10,24 @@ static int queue[HARDWARE_NUMBER_OF_FLOORS];
 static int next_order = -1;
 static int order_on_hold = -1;
 
+
+
+static void clear_all_order_lights(){
+    HardwareOrder order_types[3] = {
+        HARDWARE_ORDER_UP,
+        HARDWARE_ORDER_INSIDE,
+        HARDWARE_ORDER_DOWN
+    };
+
+    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+        for(int i = 0; i < 3; i++){
+            HardwareOrder type = order_types[i];
+            hardware_command_order_light(f, type, 0);
+        }
+    }
+}
+
+
 //Legg til i kø oppover
 
 int get_next_order(){
@@ -53,6 +71,7 @@ void clear_floor_orders(int floor){
 }
 
 void clear_all_orders(){
+    clear_all_orders();
     for (int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++){
         clear_floor_orders(floor);
     }
