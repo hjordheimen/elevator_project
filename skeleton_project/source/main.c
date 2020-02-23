37a90cac5824a867cbe1239bdff7e88f2c0639fe
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-23 01:31:49 +0100

    Rydda litt oppi main

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -43,33 +43,7 @@ int main(){
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
-    while(1){
-        /* Lights are set and cleared like this: */
-        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            /* Internal orders */
-            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
-                hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
-            }
-
-            /* Orders going up */
-            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
-                hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
-            }
-
-            /* Orders going down */
-            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
-                hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
-            }
-        }
-
-        if(hardware_read_obstruction_signal()){
-            hardware_command_stop_light(1);
-            clear_all_order_lights();
-        }
-        else{
-            hardware_command_stop_light(0);
-        }
-    }
+    initialize();
 
     while(1){
         switch (get_next_state()) {
