ba03a6853c4622c3a57ebca74fc2ab17e5bc70fc
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-27 17:57:13 +0100

    Dokumentasjon av order

--- skeleton_project/source/order.h
+++ skeleton_project/source/order.h
@@ -8,39 +8,81 @@
  * INSIDE orders to perform as a priority, but takes on orders that want to go in
  * the same direction.
  *
- * @Dependencies hardware, controller
+ * @dependencies hardware, controller
  *
  * Henrik Jordheim
  */
 
 
 
-//Neste ordre
+/**
+* @brief returns the next order (if there is any).
+*/
 
 int get_next_order();
 
+/**
+* @brief Takes the current order, and puts it on hold, and takes on the next order.
+*
+* @param new_next_order is an order of HardwareOrder
+*/
+
+
 void put_order_on_hold(int new_next_order);
+
+
+/**
+* @brief updates the orders. If there isn't any next order, see if we got any on hold.
+*/
 
 void update_next_order();
 
 
-//Bestill ordre oppover
+/**
+* @brief adds an order to a given floor.
+*
+* @param floor is the floor where the elevator is wanted
+* @param order is where the elevator should go afterwards
+*/
 
 void add_order_request(int floor, HardwareOrder order);
 
-//Slett alle ordre på en gitt etasje
+/**
+* @brief when the order is done, the floor-order is cleared (set to -1).
+*
+* @param floor is the floor to be cleared
+*/
 
 void clear_floor_orders(int floor);
 
-//Slett alle ordre
+/**
+* @brief clear all orders at every floor. Uses clear_floor_orders
+*
+*/
 
 void clear_all_orders();
 
-//Ser om det er noen inkommende ordre.
-
+/**
+* @brief polls orders, takes orders depending on the direction or INSIDE the elevator.
+* If there is an order already at a floor, nothing happens (Unless there is an INSIDE request)
+*/
 
 void get_button_signal();
 
+/**
+* @brief returns a 1 if there is more orders to dispatch, else returns 0
+*
+*/
+
 int any_requests();
+
+/**
+* @brief looks at a floor with the same direction value as the current order.
+* returns a 1 if the floor is different than the current order, else 0
+*
+* @param floor indicates the floor wich is evalueted in the direction the order is at
+* @param directionvalue is FYLLL INN MER HER!!
+*/
+
 
 int check_floor_dir_value(int floor, int directionvalue);
