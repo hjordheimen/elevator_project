9298b115fc13be15d13e58c0abb64a04e7f00998
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-03-01 09:26:37 +0100

    Stop update

--- skeleton_project/source/controller.c
+++ skeleton_project/source/controller.c
@@ -197,9 +197,10 @@ void control_stop(){
         hardware_command_movement(HARDWARE_MOVEMENT_STOP);
         order_clear_all();
         hardware_command_stop_light(1);
-        while(hardware_read_stop_signal()){
-            if(hardware_read_floor_sensor(current_floor)){
-                hardware_command_door_open(1);
+        if (hardware_read_floor_sensor(current_floor)) {
+            door_time = time(NULL);
+            hardware_command_door_open(1);
+            while (hardware_read_stop_signal()) {
                 door_time = time(NULL);
             }
         }
